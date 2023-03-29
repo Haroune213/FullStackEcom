0bede8c9c0473b75e71e45cbaad28125c02d628e
Haroune213 <123390922+Haroune213@users.noreply.github.com>
2023-03-29 11:33:51 -0400

    second commit

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,16 @@
+import storeItems from "../data/items.json"
+import { Col, Row } from 'react-bootstrap'
+import { StoreItems } from "../components/StoreItems"
+
 export function Store(){
-    return <div>
-    Store
-</div>
+    return <>
+    <h1>Store</h1>
+    <Row md={2} xs={1} lg={3}>
+        {storeItems.map(item=>(
+            <Col key={item.id}>
+                <StoreItems{...item} />
+            </Col>
+        ))}
+    </Row>
+    </>
 }
